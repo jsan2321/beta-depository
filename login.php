7f6a712cc6f558938bd75d5bd7fd2b1a60953cb4
jsan2321 <jsantiago.c@hotmail.com>
2024-01-23 22:12:44 -0500

    login added

---
+++ login.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      <title>Login</title>
+      <!-- Required meta tags -->
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+      <!-- Bootstrap CSS v5.2.1 -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+  </head>
+
+  <body>
+
+      <div class="container">
+          <div class="row">
+              <div class="col-md-4">
+
+              </div>
+
+              <div class="col-md-4">
+                  <br>
+                  <div class="card">
+                      <div class="card-header text-center">Log In</div>
+                      <div class="card-body">
+                          <form action="login.php" method="post">
+                              User: <input class="form-control" type="text" name="user" id="">
+                              <br>
+                              Password: <input class="form-control" type="text" name="password" id="">
+                              <br>
+
+                              <button type="submit" class="btn btn-outline-primary">Enter to depository</button>
+                          </form>
+                      </div>
+                      <div class="card-footer text-muted">Footer</div>
+                  </div>
+
+
+              </div>
+
+              <div class="col-md-4">
+
+              </div>
+          </div>
+      </div>
+  </body>
+
+</html>
