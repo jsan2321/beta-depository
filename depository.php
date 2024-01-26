92601c75add342c3170fd8cc84df576aa446075b
jsan2321 <jsantiago.c@hotmail.com>
2024-01-26 09:58:26 -0500

    added file uplaod and show the results of the table on the home page

--- depository.php
+++ depository.php
@@ -8,18 +8,33 @@
     if($_POST) { // Validate data from the form submmited
         $name=$_POST['name']; 
         $description=$_POST['description']; 
-        $image=$_FILES['file']['name'];
+
+        $date = new DateTime();
+
+        $image=$date->getTimestamp()."_".$_FILES['file']['name']; // date function which allows file names to be different 
+        $tempImage = $_FILES['file']['tmp_name']; // retrieves the temporary filename of the uploaded file
+        move_uploaded_file($tempImage,"images" . DIRECTORY_SEPARATOR . $image); // moves the uploaded file to the destination paht; directory and desired filename for the uploaded file
+        
         $conObj = new connection(); // get the connection
         $sql="INSERT INTO `album`.`projects` (`Name`, `Image`, `Description`) VALUES ('$name', '$image', '$description')"; 
         $conObj->execute($sql); // inserting values into the table
+
+        header("location:depository.php"); // after submitting
     }
 
     if($_GET) {
         $id=$_GET['delete'];
         $conObj = new connection();
+
+        $image = $conObj->query("SELECT `Image` FROM `projects` WHERE id=$id");
+        unlink("images/".$image[0]['Image']); // delete images from 'images' folder
+        //print_r($image[0]['Image']);
+
         $sql = "DELETE FROM `projects` WHERE `projects`.`id`=$id";
         //$sql = "DELETE FROM `album`.`projects` WHERE (`Id` = $id)";
         $conObj->execute($sql);
+
+        header("location:depository.php"); // after deleting
     }
 
     $conObj = new connection();
@@ -39,11 +54,11 @@
                     </div>
                     <div class="card-body">
                         <form action="depository.php" method="post" enctype="multipart/form-data"> <!-- enctype attribute added because a file will uploaded via post method through the form -->
-                            Project Name: <input class="form-control" type="text" name="name" id="">
+                            Project Name: <input required class="form-control" type="text" name="name" id="">
                             <br>
-                            Project Image: <input class="form-control" type="file" name="file" id="">
+                            Project Image: <input required class="form-control" type="file" name="file" id="">
                             <br>
-                            <textarea class="form-control" name="description" cols="30" rows="10"></textarea>
+                            <textarea required class="form-control" name="description" cols="30" rows="10"></textarea>
                             <br>
                             <input type="submit" value="Send project" class="btn btn-success">
                         </form>
@@ -68,7 +83,9 @@
                         <tr>
                             <td><?php echo $project['Id'] ?></td>
                             <td><?php echo $project['Name'] ?></td>
-                            <td><?php echo $project['Image'] ?></td>
+                            <td>
+                                <img width="100" src="images/<?php echo $project['Image']?>" alt="" srcset="">
+                            </td>
                             <td><?php echo $project['Description'] ?></td>
                             <td><a class="btn btn-danger" href="?delete=<?php echo $project['Id']?>">Delete</a></td>
                         </tr>
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php include("header.php") ?>
+<?php include("connection.php") ?>
 
+  <?php
+      $conObj = new connection();
+      $projects = $conObj->query("SELECT * FROM `projects`");
+  ?>
     
     <div class="p-5 mb-4 bg-body-tertiary rounded-5">
       <div class="container-fluid py-5">
@@ -9,6 +14,19 @@
         <button type="button" class="btn btn-outline-primary">More info</button>
       </div>
     </div>
-    
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <?php foreach($projects as $project) { ?>
+        <div class="col">
+          <div class="card">
+            <img width="50" src="images/<?php echo $project['Image']?>" class="card-img-top" >
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $project['Name'] ?></h5>
+              <p class="card-text"><?php echo $project['Description'] ?></p>
+            </div>
+          </div>
+        </div>
+      <?php } ?>
+    </div>
 
 <?php include("footer.php") ?>
