a48fef111f16270a859c6a4ca531880b8b15c8c7
José Santiago C <144196161+jsan2321@users.noreply.github.com>
2024-01-26 10:07:07 -0500

    Update projects.sql

--- database/projects.sql
+++ database/projects.sql
@@ -14,3 +14,13 @@ CREATE TABLE photos(
 
 -- Just changes table name :p
 ALTER TABLE photos RENAME projects;
+
+
+-- Insert data fields
+INSERT INTO `album`.`projects` (`Name`, `Image`, `Description`) VALUES ('Task Manager', '$image', 'Simple task manager that allows users to add, edit, and delete tasks developed in Python')";
+
+INSERT INTO `album`.`projects` (`Name`, `Image`, `Description`) VALUES ('Simple Game', '$image', 'Classic Tic-Tac-Toe game developed in Java')"; 
+
+INSERT INTO `album`.`projects` (`Name`, `Image`, `Description`) VALUES ('Blog Platform', '$image', 'Basic blogging platform where users can create, edit, and delete posts developed in JavaScript')"; 
+
+INSERT INTO `album`.`projects` (`Name`, `Image`, `Description`) VALUES ('Personal Portfolio Website', '$image', 'SPersonal website to showcase my projects, skills, and resume developed in PHP')"; 
